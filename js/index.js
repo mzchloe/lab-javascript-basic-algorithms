9cfcadd36ba02918efe01f685d85cbea1ea0ad7c
Jeanet Lin <linett@gmail.com>
2021-11-13 00:48:35 +0100

    added 3.3

--- js/index.js
+++ js/index.js
@@ -1,13 +1,16 @@
 console.log("I'm ready1");
 // Iteration 1: Names and Input
 
+//1.1
 let hacker1 = "Ben"
-
-
+ 
+//1.2
 console.log(`The driver's name is ${hacker1}`);
 
+//1.3
 let hacker2 = "Jerry"
 
+//1.4
 console.log(`The driver's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
@@ -25,7 +28,7 @@ if (hacker1.length > hacker2.length){
   }
 // Iteration 3: Loops
 
-
+//3.1
 
 let driverName = '';
 
@@ -36,6 +39,7 @@ for (let characters of hacker1) {
 
 console.log(driverName)
 
+//3.2
 let reversed = '';
 
 for (let character of hacker2){
@@ -45,7 +49,13 @@ for (let character of hacker2){
 
 console.log(reversed)
 
-
+//3.3
+if (hacker1 < hacker2) {
+    console.log(`The driver's name goes first`);
+  } else if (hacker1 > hacker2) {
+    console.log('Yo, the navigator goes first definitely.');
+  } else console.log('What?! You both have the same name?');
+  
 
 
 
